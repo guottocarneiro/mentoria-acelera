b5d2abd0f290b0f3d37d5ae3e564fcd4a9fe17fa
guottocarneiro <40148910+guottocarneiro@users.noreply.github.com>
2025-06-13 12:33:06 +0000

    Fix routes path

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // app.js / 
 import express from 'express'
-import TarefaRoutes from '../tarefaRoutes.js'
+import TarefaRoutes from './tarefaRoutes.js'
 
 const app = express();
 const PORT = process.env.PORT || 3000;
